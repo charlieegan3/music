61c4409a77e7be083f485c5129bf6b6434136742
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-10-05 22:18:33 +0100

    Add exponential backoff to enrich & backup

--- cmd/backup.go
+++ cmd/backup.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"log"
+	"time"
 
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/charlieegan3/music/internal/pkg/backup"
 	"github.com/spf13/cobra"
 )
@@ -16,7 +18,11 @@ var backupPlaysCommand = &cobra.Command{
 	Use:   "plays",
 	Short: "save data in bigquery table to GCS",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := backup.Plays(cfg)
+		b := backoff.NewExponentialBackOff()
+		b.MaxElapsedTime = 3 * time.Minute
+		err := backoff.Retry(func() error {
+			return backup.Plays(cfg)
+		}, b)
 		if err != nil {
 			log.Fatalf("backup failed: %v", err)
 		}
--- cmd/enrich.go
+++ cmd/enrich.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"log"
+	"time"
 
+	backoff "github.com/cenkalti/backoff/v4"
 	"github.com/charlieegan3/music/internal/pkg/enrich"
 	"github.com/spf13/cobra"
 )
@@ -11,7 +13,11 @@ var enrichCommand = &cobra.Command{
 	Use:   "enrich",
 	Short: "enrich raw play data and reupload",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := enrich.Enrich(cfg)
+		b := backoff.NewExponentialBackOff()
+		b.MaxElapsedTime = 3 * time.Minute
+		err := backoff.Retry(func() error {
+			return enrich.Enrich(cfg)
+		}, b)
 		if err != nil {
 			log.Fatalf("enrich failed: %v", err)
 		}
